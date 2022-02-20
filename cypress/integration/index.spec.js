7b49263344b01fd087a4f09ef14e69648f6602e5
mclbdn <michalbednar93@gmail.com>
2022-02-20 12:31:09 -0600

    Add cypress tests

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,73 @@
+describe("Index page tests", () => {
+  // Response status tests
+  describe("Response status tests", () => {
+    it("Loads the index page successfully", () => {
+      cy.request({ url: "/" }).then((resp) => {
+        expect(resp.status).to.eq(200);
+      });
+    });
+  });
+
+  // Visit index page before each test case
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  // Header part tests
+  describe("Header part tests", () => {
+    it("Renders hero section correctly", () => {
+      cy.get(".hero-container").should("exist");
+    });
+  });
+
+  // Feature part tests
+  describe("Feature part tests", () => {
+    it("Renders feature section correctly", () => {
+      cy.get(".features-section").should("exist");
+    });
+  });
+
+  // Contact part tests
+  describe("Contact part tests", () => {
+    it("Renders contact section correctly", () => {
+      cy.get(".contact-section").should("exist");
+    });
+
+    it("Allows the form to be used", () => {
+      cy.get("#contact-form").within(() => {
+        // Name input
+        cy.get("input[name='name']")
+          .should("exist")
+          .type("Michal")
+          .and("have.value", "Michal");
+
+        // Email input
+        cy.get("input[name='email']")
+          .should("exist")
+          .type("michal@email.com")
+          .and("have.value", "michal@email.com");
+
+        // Phone input
+        cy.get("input[name='phone']")
+          .should("exist")
+          .type("123456789")
+          .and("have.value", "123456789");
+      });
+
+      // Message textarea
+      cy.get("textarea[name='message']")
+        .should("exist")
+        .type("Hi, there!")
+        .and("have.value", "Hi, there!");
+
+      // Submit button
+      cy.get("button[type='submit']").should("exist").click();
+
+      // Fields cleared
+      cy.get("input[name='name']").should("have.value", "");
+      cy.get("input[name='email']").should("have.value", "");
+      cy.get("input[name='phone']").should("have.value", "");
+      cy.get("textarea[name='message']").should("have.value", "");
+    });
+  });
+});
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,19 +11,20 @@ const ContactForm = () => {
     <form action="" id="contact-form" onSubmit={handleSubmit}>
       <h3>Contact Us</h3>
       <fieldset>
-        <input placeholder="NAME" type="text" tabindex="1" required />
+        <input placeholder="NAME" name="name" type="text" tabindex="1" required />
       </fieldset>
       <fieldset>
-        <input placeholder="EMAIL" type="email" tabindex="2" required />
+        <input placeholder="EMAIL" name="email" type="email" tabindex="2" required />
       </fieldset>
       <fieldset>
-        <input placeholder="PHONE NUMBER" type="tel" tabindex="3" required />
+        <input placeholder="PHONE NUMBER" name="phone" type="tel" tabindex="3" required />
       </fieldset>
       <fieldset>
         <textarea
           placeholder="Type your Message Here...."
           tabindex="5"
           required
+          name="message"
         ></textarea>
       </fieldset>
       <fieldset>
